e61c0d7b1f89c5d9c6b1b311d53f44e3b53b78ac
zakonik <zakonik2417@gmail.com>
2021-05-17 12:46:59 -0400

    added queries.js for GET_ME and setup mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput!) {
+    addUser(book: $book) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+            image
+            link
+        }
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation saveBook($bookId: String!) {
+    addUser(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+            image
+            link
+        }
+    }
+  }
+`;
